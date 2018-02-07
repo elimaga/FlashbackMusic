bc5f12ded01f2f8d6572b8edabbf01a753f45a9c
Kazutaka Homma <special18fender@gmail.com>
2018-02-06 16:48:57 -0800

    add working tabs

---
+++ FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/TabFragment1.java
@@ -0,0 +1,21 @@
+package com.example.team13.flashbackmusic;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kazutaka on 2/6/18.
+ */
+
+public class TabFragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+
+    }
+}
---
+++ FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/TabFragment2.java
@@ -0,0 +1,19 @@
+package com.example.team13.flashbackmusic;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kazutaka on 2/6/18.
+ */
+
+public class TabFragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+
+    }
+}
