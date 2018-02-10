67ce0ad56de33547a4752bebd14d5ac56ed6ba3b
andrewyu <yuandrew8557@gmail.com>
2018-02-10 14:31:35 -0800

    Added methods to gather user info

--- FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/MainActivity.java
+++ FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.team13.flashbackmusic;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +17,12 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    static final int REQUEST_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +67,95 @@ public class MainActivity extends AppCompatActivity {
                 // TODO: Please remove the following line
                 Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
 
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                double[] userLocation = getLocation();
+                String userTime = getTime();
+                String userDay = getDay();
             }
         });
     }
 
+    private String getDay()
+    {
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+        switch(day)
+        {
+            case Calendar.SUNDAY:
+                return "Sunday";
+            case Calendar.MONDAY:
+                return "Monday";
+            case Calendar.TUESDAY:
+                return "Tuesday";
+            case Calendar.WEDNESDAY:
+                return "Wednesday";
+            case Calendar.THURSDAY:
+                return "Thursday";
+            case Calendar.FRIDAY:
+                return "Friday";
+            case Calendar.SATURDAY:
+                return "Saturday";
+        }
+
+        return "";
+    }
+
+    private String getTime()
+    {
+        int hours = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);//currentTime.getHours();
+        int mins = Calendar.getInstance().get(Calendar.MINUTE);//currentTime.getMinutes();
+
+        return hours + ":" + mins;
+    }
+
+    private double[] getLocation()
+    {
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if  (ActivityCompat.checkSelfPermission ( this , Manifest.permission.ACCESS_FINE_LOCATION )
+                != PackageManager.PERMISSION_GRANTED  && ActivityCompat.checkSelfPermission ( this ,
+                Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+            ActivityCompat.requestPermissions ( this ,
+                    new  String[]{Manifest.permission.ACCESS_FINE_LOCATION },  REQUEST_LOCATION );
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null)
+        {
+            double[] newLocation = {location.getLatitude(), location.getLongitude()};
+            return newLocation;
+        }
+        else {
+            double[] newLocation = {0.0, 0.0};
+            return newLocation;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
