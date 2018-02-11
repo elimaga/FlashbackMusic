c2c8afe0da3d30a9e8b45fc7a28ed49bb2777d3f
lbatoon <luzanne.bat@gmail.com>
2018-02-11 14:20:10 -0800

    Minor changes

--- FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/Song.java
+++ FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/Song.java
@@ -10,19 +10,19 @@ import java.sql.Time;
 
 /* SONG CLASS */
 public class Song {
-    private String title, artist, albumName, duration, path, track, lastTime, setting;
-    private Location lastLocation;
+    private String title, artist, albumName, path, track, lastTime, setting;
+    private double lastLatitude, lastLongitude;
     private int liked; // neutral = 0, dislike = -1, like = 1
 
     // What type to store location and time?
 //    private Location lastLocation;
 //    private Time lastTime;
 
-    public Song(String title, String artist, String albumName, String duration, String path, String track) {
+    public Song(String title, String artist, String albumName, String duration,
+                String path, String track) {
         this.title = title;
         this.artist = artist;
         this.albumName = albumName;
-        this.duration = duration;
         this.path = path;
         this.track = track;
         this.liked = 0;
@@ -48,15 +48,6 @@ public class Song {
         return this.albumName;
     }
 
-    public String getDuration() {
-        long numduration = Long.parseLong(duration)/1000;
-        long h = numduration /3600;
-        long m = (numduration - h * 3600) / 60;
-        long sec = numduration - (h * 3600 + m * 60);
-        String durationText = "" + m + ":" + sec;
-        return durationText;
-    }
-
     public String getPath() {
         return this.path;
     }
@@ -67,18 +58,25 @@ public class Song {
         this.liked = like;
     }
 
-    private Location getLastLocation() {
-        return this.lastLocation;
+    private double getLastLatitude() {
+        return this.lastLatitude;
+    }
+
+    private double getLastLongitude() {
+        return this.lastLongitude;
     }
 
     private String getLastTime() {
         return this.lastTime;
     }
 
-    private void setData(Location location, String time) {
+    private void setData(double lastLatitude, double lastLongitude,
+                         String time) {
         this.lastTime = time;
-        this.lastLocation = location;
-        int hour = Integer.parseInt(time.substring(0, 2));
+        this.lastLatitude = lastLatitude;
+        this.lastLongitude = lastLongitude;
+
+        int hour = Integer.parseInt(time.substring(0, time.indexOf((":"))));
         if ((hour >= 5) && (hour < 11))
             this.setting = "Morning";
         else if ((hour >=11) && (hour < 17))
