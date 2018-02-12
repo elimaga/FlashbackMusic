3d2e9fc579342c38af7f1fa66e2f699c4636cbdf
Kazutaka Homma <special18fender@gmail.com>
2018-02-11 17:35:30 -0800

    define onclick function

--- FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/FlashbackActivity.java
+++ FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/FlashbackActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class FlashbackActivity extends AppCompatActivity {
 
@@ -14,8 +15,13 @@ public class FlashbackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashback);
 
-
-
+        Button close = findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
 }
