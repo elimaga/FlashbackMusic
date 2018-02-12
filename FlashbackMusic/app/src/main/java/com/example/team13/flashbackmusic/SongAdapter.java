f8b3654b23e1cc93b2eb2c656ad6b9144bb5aba1
Kazutaka Homma <special18fender@gmail.com>
2018-02-11 16:05:46 -0800

    rename list_item_song to list_item

--- FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/SongAdapter.java
+++ FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/SongAdapter.java
@@ -4,13 +4,9 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 /**
@@ -46,7 +42,7 @@ public class SongAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
-        View rowView = mInflater.inflate(R.layout.list_item_song, parent, false);
+        View rowView = mInflater.inflate(R.layout.list_item, parent, false);
 
         TextView titleTextView = rowView.findViewById(R.id.title);
 
