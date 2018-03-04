fd8e3d1447cf0584fdb877832d3c933d8fe624de
kmloh <kmloh@ucsd.edu>
2018-03-03 20:14:56 -0800

    add ic_menu .xml drawer_view.xml menu_layout.xml

--- FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/MainActivity.java
+++ FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/MainActivity.java
@@ -15,14 +15,19 @@ import android.location.LocationManager;
 import android.support.annotation.NonNull;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -34,6 +39,7 @@ import java.lang.reflect.Field;
 public class MainActivity extends AppCompatActivity {
 
     ImageButton flashBackButton;
+    private DrawerLayout mDrawerLayout;
 
     private ArrayList<Song> songs;
     private ArrayList<Album> albums;
@@ -49,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
         // Load the songs and albums into the ArrayLists
         MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
@@ -81,6 +90,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
 
 
         // Flashback button
@@ -122,6 +150,16 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions ( this ,
                     new  String[]{Manifest.permission.ACCESS_FINE_LOCATION },  REQUEST_LOCATION );
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public Song getSong(int index)
