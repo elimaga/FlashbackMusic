c814ef5246caab210e49c55c3a79f5fa3af5799e
lbatoon <luzanne.bat@gmail.com>
2018-03-16 19:15:59 -0700

    add missing files

--- FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/MainActivity.java
+++ FlashbackMusic/app/src/main/java/com/example/team13/flashbackmusic/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
     final int INVALID_COORDINATE = 200;
     private GoogleUtility googleUtility;
     public FBMUser usr;
+    private boolean isEspresso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +68,12 @@ public class MainActivity extends AppCompatActivity {
 
         googleUtility = new GoogleUtility(MainActivity.this, this);
         GoogleSignInAccount acct = googleUtility.getLastAccount();
-        usr = new FBMUser(acct.getId(), acct.getDisplayName());
+        if(isEspresso) {
+            usr = new FBMUser("1", "Test User");
+        } else {
+            usr = new FBMUser(acct.getId(), acct.getDisplayName());
+        }
+
         googleUtility.setUser(usr);
         Log.d("MainActivity", "Created user: " + usr.getName());
 
