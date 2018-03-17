c88f08164b7a536472c3a74fa5d5a8b1e437a1cf
rolandkong <rolandkong7@gmail.com>
2018-03-16 21:22:45 -0700

    Added test for downloading a single song

--- FlashbackMusic/app/src/androidTest/java/com/example/team13/flashbackmusic/DownloadTest.java
+++ FlashbackMusic/app/src/androidTest/java/com/example/team13/flashbackmusic/DownloadTest.java
@@ -1,48 +1,50 @@
-package tests;
+package com.example.team13.flashbackmusic;
 
 
-import android.content.Intent;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.support.test.uiautomator.UiDevice;
-import android.support.test.uiautomator.UiObject;
-import android.support.test.uiautomator.UiSelector;
-import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
+        import android.content.Intent;
+        import android.support.test.InstrumentationRegistry;
+        import android.support.test.espresso.ViewInteraction;
+        import android.support.test.espresso.matcher.ViewMatchers;
+        import android.support.test.rule.ActivityTestRule;
+        import android.support.test.runner.AndroidJUnit4;
+        import android.support.test.uiautomator.UiDevice;
+        import android.support.test.uiautomator.UiObject;
+        import android.support.test.uiautomator.UiSelector;
+        import android.test.suitebuilder.annotation.LargeTest;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.view.ViewParent;
+        import android.widget.TextView;
 
-import com.example.team13.flashbackmusic.MainActivity;
-import com.example.team13.flashbackmusic.R;
+        import com.example.team13.flashbackmusic.MainActivity;
+        import com.example.team13.flashbackmusic.R;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+        import org.hamcrest.Description;
+        import org.hamcrest.Matcher;
+        import org.hamcrest.TypeSafeMatcher;
+        import org.hamcrest.core.IsInstanceOf;
+        import org.junit.Before;
+        import org.junit.Rule;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
+        import static android.support.test.espresso.Espresso.onView;
+        import static android.support.test.espresso.action.ViewActions.click;
+        import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+        import static android.support.test.espresso.action.ViewActions.replaceText;
+        import static android.support.test.espresso.assertion.ViewAssertions.matches;
+        import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
+        import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+        import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+        import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+        import static android.support.test.espresso.matcher.ViewMatchers.withId;
+        import static android.support.test.espresso.matcher.ViewMatchers.withText;
+        import static org.hamcrest.Matchers.allOf;
+        import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class DownLoadSBST {
+public class DownloadTest {
     private UiDevice uiDevice;
 
     @Rule
